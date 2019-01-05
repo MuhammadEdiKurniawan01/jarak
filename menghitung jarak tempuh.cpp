34f7967a2b2f9d9c6ef398ef40caee113a5c6eec
MuhammadEdiKurniawan01 <edikurunyawan25@gmail.com>
2019-01-05 21:04:36 +0700

    projok 5

---
+++ menghitung jarak tempuh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float v,t,jarak;
+
+    cout<<"masukan kecepatan benda : ";
+    cin>>v;
+    cout<<"masukan waktu yang diperlukan : ";
+    cin>>t;
+    cout<<"           S = v x t\n";
+    jarak=v*t;
+    cout<<"jarak tempuh yang d peroleh adalah :"<<jarak;
+    return 0;
+
+}
---
+++ menghitung jarak tempuh.h
@@ -0,0 +1,16 @@
+#ifndef MENGHITUNG JARAK TEMPUH_H
+#define MENGHITUNG JARAK TEMPUH_H
+
+
+class menghitung jarak tempuh
+{
+    public:
+        menghitung jarak tempuh();
+        virtual ~menghitung jarak tempuh();
+
+    protected:
+
+    private:
+};
+
+#endif // MENGHITUNG JARAK TEMPUH_H
